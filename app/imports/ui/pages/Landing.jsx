24ae565c4c0a39c625815e4a05d68a75489b9be8
Gaila Galano <gailag@hawaii.edu>
2022-07-13 22:51:45 -0400

    Added tabs on landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Grid, Tab } from 'semantic-ui-react';
+import { Tab } from 'semantic-ui-react';
 
-/** A simple static component to render some text for the landing page. */
-class Landing extends React.Component {
+const panes = [
+  { menuItem: 'High', render: () => <Tab.Pane color='red' >Tab 1 Content</Tab.Pane> },
+  { menuItem: 'Medium', render: () => <Tab.Pane color='yellow' >Tab 2 Content</Tab.Pane> },
+  { menuItem: 'Low', render: () => <Tab.Pane color='green' >Tab 3 Content</Tab.Pane> },
+];
 
-  const panes = [
-    { menuItem: 'Tab 1', render: () => <Tab.Pane>Tab 1 Content</Tab.Pane> },
-    { menuItem: 'Tab 2', render: () => <Tab.Pane>Tab 2 Content</Tab.Pane> },
-    { menuItem: 'Tab 3', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
-  ]
-
-  render() {
-    return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
-
-      </Grid>
-    );
-  }
-}
+const Landing = () => (
+  <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
+);
 
 export default Landing;
