dbda6e26da2bb8af700599104469ccf1cd8c9dc6
Gaila Galano <gailag@hawaii.edu>
2022-07-14 23:11:20 -0400

    fixed tabs and added header

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Tab } from 'semantic-ui-react';
+import { Tab, Header, Container } from 'semantic-ui-react';
 
-const panes = [
-  { menuItem: 'High', render: () => <Tab.Pane color='red' >Tab 1 Content</Tab.Pane> },
-  { menuItem: 'Medium', render: () => <Tab.Pane color='yellow' >Tab 2 Content</Tab.Pane> },
-  { menuItem: 'Low', render: () => <Tab.Pane color='green' >Tab 3 Content</Tab.Pane> },
-];
+class Landing extends React.Component {
+  render() {
+    const panes = [
+      { menuItem: { color: 'red', content: 'High' },
+        render: () => <Tab.Pane color='red'>Tab 1 Content</Tab.Pane>,
+      },
+      { menuItem: { color: 'yellow', content: 'Medium' },
+        render: () => <Tab.Pane color='yellow'>Tab 2 Content</Tab.Pane>,
+      },
+      { menuItem: { color: 'green', content: 'Low' },
+        render: () => <Tab.Pane color='green'>Tab 3 Content</Tab.Pane> },
+    ];
 
-const Landing = () => (
-  <Tab menu={{ fluid: true, vertical: true, tabular: true }} panes={panes} />
-);
+    /* const Landing = () => ( */
+    return (
+      <Container>
+        <Header size='huge' textAlign='center'>Platoon Mental Health Analysis</Header>
+        <Tab
+          menu={{ fluid: true, vertical: true }}
+          menuPosition='left'
+          panes={panes}
+        />
+      </Container>
+    );
+  }
+}
 
 export default Landing;
